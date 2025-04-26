c819086e0c124d0a2e919aad13432b7874d3768f
aliafify <ali.afify93746@gmail.com>
2025-04-26 05:55:56 +0300

    redeploy server

--- src/common/constants/path.constant.ts
+++ src/common/constants/path.constant.ts
@@ -1,9 +1,9 @@
 import { join } from 'path';
 
 export const ROOT_PATH = process.cwd();
-export const UPLOAD_PATH = join(ROOT_PATH, 'uploads');
-// export const CLIENT_SIDE_PATH = join('/root/actions-runner/_work/Rakez_Faker-front/Rakez_Faker-front', 'build');
-export const CLIENT_SIDE_PATH = join(ROOT_PATH, 'build');
+export const UPLOAD_PATH = join('/home/osbash/', 'uploads');
+export const CLIENT_SIDE_PATH = join('/home/osbash/actions-runner/_work/Rakez_Faker-front/Rakez_Faker-front', 'build');
+// export const CLIENT_SIDE_PATH = join(ROOT_PATH, 'build');
 // export const ADMIN_SIDE_PATH = join('/root/actions-runner/_work/faker_rakez_admin_dash/faker_rakez_admin_dash', 'dist');
 export const ADMIN_SIDE_PATH = join(ROOT_PATH, 'dist');
 export const VIEW_PATH = join(ROOT_PATH, 'views');
